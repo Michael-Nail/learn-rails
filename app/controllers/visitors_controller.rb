61423278449c5859cbf3656462bb52f4f1485c04
Michael Nail <MichaelAlexNail@me.com>
2014-04-08 07:38:00 -0400

    Add flash messages.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
 	def new
         Rails.logger.debug 'DEBOG: entering new method'
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome'
+		flash.now[:alert] = 'My birthday is soon.'
         Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 	end
 
