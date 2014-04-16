c1d5cab29015f2edeb81ea24989f37141439061a
Michael Nail <MichaelAlexNail@me.com>
2014-04-16 02:42:12 -0400

    Remove test code from visitors_controller.rb

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,7 @@
 class VisitorsController < ApplicationController
 
 	def new
-        Rails.logger.debug 'DEBOG: entering new method'
 		@owner = Owner.new
-		flash.now[:notice] = 'Welcome'
-		flash.now[:alert] = 'My birthday is soon.'
-        Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 	end
 
 end
